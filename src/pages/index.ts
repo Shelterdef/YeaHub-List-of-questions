3087b3a6d57585b022f37f50b62eeff5601df6b5
Shelterdef <shelter.def@yandex.ru>
2025-11-04 22:40:57 +0300

    chore: add path aliases and standardize imports

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+// src\pages\index.ts
+export { HomePage } from "./home";
+export { NotFoundPage } from "./not-found";
+export { QuestionPage } from "./question-page";
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,3 +2,9 @@
 export { ListQuestions } from "./ListQuestions/ui/ListQuestions";
 export { SortingQues } from "./SortingQues/ui/SortingQues";
 export { Layout, Header } from "./layout";
+export { QuestionAnswer } from "./question-content";
+export { QuestionCode } from "./question-content";
+export { QuestionContent } from "./question-content";
+export { QuestionHeader } from "./question-content";
+export { QuestionCard } from "./questionCard";
+export { QuestionSidebar } from "./QuestionSidebar";
